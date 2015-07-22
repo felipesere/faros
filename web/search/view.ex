c413a7970f0a635fb86c819ada17fb944bdf10d1
Felipe Sere <felipesere@gmail.com>
2015-07-22 22:32:40 +0100

    Make search results links to their respective entity.

--- web/search/view.ex
+++ web/search/view.ex
@@ -1,4 +1,13 @@
 defmodule Lighthouse.Search.View do
   use Lighthouse.Web, :view
+  alias Lighthouse.Books.Book
+  alias Lighthouse.Papers.Paper
 
+  def url_for(conn, %Book{ slug: slug }) do
+    books_path(conn, :show, slug)
+  end
+
+  def url_for(conn, %Paper{ slug: slug }) do
+    papers_path(conn, :show, slug)
+  end
 end
