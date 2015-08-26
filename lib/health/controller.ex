c7d7ea5e552322d5c618e58b9b968fa36c832da2
Felipe Sere <felipesere@gmail.com>
2015-08-26 20:36:20 +0100

    The module attribute @last_sha is not needed due to macro expansion

--- lib/health/controller.ex
+++ lib/health/controller.ex
@@ -2,23 +2,21 @@ defmodule Lighthouse.Health.Controller do
   use Lighthouse.Web, :controller
   require Git
 
-  @last_sha Git.last
-
   def check(conn, _params) do
     checks = %{ :database => db_check }
     status = to_status(checks)
-    result = %{:sha => @last_sha, :checks => checks}
+    result = %{:sha => Git.last, :checks => checks}
 
     conn
     |> put_status(status)
     |> json(result)
   end
 
-  def to_status(results) do
-    results
+  def to_status(check) do
+    check
     |> Enum.reduce(true, fn({_,value}, acc) -> acc && value end)
     |> case do
-      true -> 200
+      true  -> 200
       false -> 500
     end
   end
