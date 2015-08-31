9d1a160ec49859bb0566532aea54e5c0729ff0ab
Felipe Sere <felipesere@gmail.com>
2015-08-31 23:15:34 +0200

    Add update scipt

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+aws --version
+aws configure set default.region us-west-2
+aws configure set default.output json
+
+echo "preparing task definition"
+aws ecs describe-task-definition --task-definition applications | ./jq --arg x $CIRCLE_SHA1 ' .taskDefinition
+                                                                                              | del(.status)
+                                                                                              | del(.taskDefinitionArn)
+                                                                                              | del(.revision)
+                                                                                              | .containerDefinitions[0].image = ("felipesere/lighthouse:"+$x)' > new-task-definition.json
+
+NEW_REVISION=$(aws ecs register-task-definition --cli-input-json file://new-task-definition.json | ./jq '.taskDefinition.revision')
+
+echo "updating service definition"
+aws ecs update-service --cluster alexandria --service lighthouse-2 --task-definition "applications:$NEW_REVISION"
+
+
