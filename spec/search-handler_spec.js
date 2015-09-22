ea51d7c20d749993cd1ed9f14eb67d824f07a5e7
Felipe Sere <felipesere@gmail.com>
2015-09-22 12:17:41 +0100

    Does not jump into search if input is focused

--- spec/search-handler_spec.js
+++ spec/search-handler_spec.js
@@ -7,16 +7,31 @@ import {SearchHandler} from 'web/static/js/search-handler.js'
 describe('Search-Handler', () => {
   beforeEach(() => {
     affix('input[data-id=search]')
+    affix('form input#keeps-focus input[type=text]')
   })
+
 
   it('focuses when key pressed', () => {
     SearchHandler.bindEvents('search')
-    let pressS = jQuery.Event('keyup', { keyCode: 83, which: 83 })
 
-    $(document).trigger(pressS)
+    pressS($(document))
 
     expect('[data-id="search"]').toBeFocused()
   })
+
+  it('does not move focus away', () => {
+    SearchHandler.bindEvents('search')
+    $('#keeps-focus').focus()
+
+    pressS($('#keep-focus'))
+
+    expect('[data-id="search"]').not.toBeFocused()
+  })
+
+  function pressS(where) {
+    var event = jQuery.Event('keyup', { keyCode: 83, which: 83 })
+    where.trigger(event)
+  }
 })
 
 
--- web/static/js/search-handler.js
+++ web/static/js/search-handler.js
@@ -3,6 +3,10 @@ export var SearchHandler = {
     let targetSelector = `[data-id='${target}']`
 
     $(document).keyup((e) => {
+      if( $(e.target).is( 'input' )) {
+        return;
+      }
+
       let code = e.which ? e.which : e.keyCode
 
       if(83 === code) {
