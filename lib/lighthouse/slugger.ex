b1b7b93d30cce467a01d9f318c82b00778b5d347
Felipe Sere <felipesere@gmail.com>
2015-10-01 14:31:20 +0100

    Use methods in to explain how a slug is generated

--- lib/lighthouse/slugger.ex
+++ lib/lighthouse/slugger.ex
@@ -2,7 +2,15 @@ defmodule Lighthouse.Slugger do
   def generate(title) do
     title
     |> String.downcase
-    |> String.replace(" ","-")
-    |> String.replace(~r/[^\w-]/,"")
+    |> replace_whitespace
+    |> remove_non_word_charactes
+  end
+
+  def replace_whitespace(title) do
+    String.replace(title, " ","-")
+  end
+
+  def remove_non_word_charactes (title) do
+    String.replace(title, ~r/[^\w-]/,"")
   end
 end
