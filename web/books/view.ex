f3b73c2f1e2cd3ebcacc9150dff391d9abbf2386
Felipe Sere <felipesere@gmail.com>
2015-10-03 19:38:02 +0100

    Style conflicting slugs differnetly

--- web/books/view.ex
+++ web/books/view.ex
@@ -4,4 +4,12 @@ defmodule Faros.Books.View do
   def render("lookup.json", %{book: book}) do
     %{book: book}
   end
+
+  def style(%Ecto.Changeset{ errors: errors }, field) do
+    if errors[field] do
+      "error-value"
+    else
+      ""
+    end
+  end
 end
