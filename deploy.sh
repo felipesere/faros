807cf2bf5cdbd72597f9a0c10d51ad6afde744af
Felipe Sere <felipesere@gmail.com>
2016-02-06 23:21:35 +0000

    Update deploy script to reflect latest infrastructure names

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ aws configure set default.region us-west-2
 aws configure set default.output json
 
 echo "preparing task definition"
-aws ecs describe-task-definition --task-definition applications | ./jq --arg x $CIRCLE_SHA1 ' .taskDefinition
-                                                                                              | del(.status)
-                                                                                              | del(.taskDefinitionArn)
-                                                                                              | del(.revision)
-                                                                                              | .containerDefinitions[0].image = ("felipesere/faros:"+$x)' > new-task-definition.json
+aws ecs describe-task-definition --task-definition applications-2 | ./jq --arg x $CIRCLE_SHA1 ' .taskDefinition
+                                                                                                | del(.status)
+                                                                                                | del(.taskDefinitionArn)
+                                                                                                | del(.revision)
+                                                                                                | .containerDefinitions[0].image = ("felipesere/faros:"+$x)' > new-task-definition.json
 
 NEW_REVISION=$(aws ecs register-task-definition --cli-input-json file://new-task-definition.json | ./jq '.taskDefinition.revision')
 
 echo "updating service definition"
-aws ecs update-service --cluster alexandria --service faros --task-definition "applications:$NEW_REVISION"
+aws ecs update-service --cluster alexandria --service faros-2 --task-definition "applications-2:$NEW_REVISION"
 
 
