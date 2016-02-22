e8864fa0c4480cfcd6e3cda2d5c1a73869c8dd73
Chris Jordan <cjordan@8thlight.com>
2016-02-22 19:57:37 +0000

    Playing around with London underground colours.
    Needs work.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule Faros.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def cj_styleguide(conn, _params) do
+    render conn, "cj_styleguide.html"
+  end
 end
